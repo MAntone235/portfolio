2f14a5f2b80f525fe1f7609f370e9f5d0b8cdd98
MAntone235 <margaretantone17@gmail.com>
2024-09-11 12:59:03 -0400

    changes to homepage and about

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const navMenu = document.getElementById('nav-menu');
+    if (navMenu.style.display === 'block') {
+        navMenu.style.display = 'none';
+    } else {
+        navMenu.style.display = 'block';
+    }
+}
